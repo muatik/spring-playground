a9c217997775b90378d88795dde6a5d32ec8fe1d
muatik <muatik@gmail.com>
2017-01-07 15:11:56 +0200

    adds spring-jpa pagination example

--- spring-jpa-example-1/src/main/java/com/muatik/Application.java
+++ spring-jpa-example-1/src/main/java/com/muatik/Application.java
@@ -1,6 +1,7 @@
 package com.muatik;
 
 import com.muatik.model.Person;
+import com.muatik.repository.PersonPagingAndSortingRepository;
 import com.muatik.repository.PersonRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -9,6 +10,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.domain.PageRequest;
 
 /**
  * Created by muatik on 1/7/17.
@@ -18,6 +20,9 @@ import org.springframework.context.annotation.Bean;
 public class Application {
 
     private static final Logger logger = LoggerFactory.getLogger(Application.class);
+
+    @Autowired
+    PersonPagingAndSortingRepository personPagingAndSortingRepository;
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
@@ -29,12 +34,20 @@ public class Application {
             repository.save(new Person(75, "mustafa"));
             repository.save(new Person(74, "john"));
             repository.save(new Person(71, "jane"));
+            repository.save(new Person(73, "neval"));
+            repository.save(new Person(69, "micheal"));
+            repository.save(new Person(81, "mateo"));
 
             logger.info("");
             logger.info("Count: " + repository.count());
 
             repository.findAllByHeight(71).forEach(each -> {
                 logger.info(each.getName() + ", whose height is 71");
+            });
+
+            logger.info("== second page with page size 4 ==");
+            personPagingAndSortingRepository.findAll(new PageRequest(1, 4)).forEach(each -> {
+                logger.info(each.getName());
             });
 
         };
