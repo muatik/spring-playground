979c9ca7e5518765be2bf65893449dc57433f248
muatik <muatik@gmail.com>
2017-01-07 22:40:47 +0200

    ads limiting queries in spring-jpa-example

--- spring-jpa-example-1/src/main/java/com/muatik/Application.java
+++ spring-jpa-example-1/src/main/java/com/muatik/Application.java
@@ -12,6 +12,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 
 
 /**
@@ -42,6 +43,9 @@ public class Application {
             repository.save(new Person(75, "micheal", "bourbone", new Address("San Francisco", "US")));
             repository.save(new Person(81, "mateo", "senza", new Address("Rome", "Italy")));
             repository.save(new Person(79, "mehmet", "yilmaz", new Address("Istanbul", "Turkey")));
+            repository.save(new Person(72, "sinan", "sar", new Address("Istanbul", "Turkey")));
+            repository.save(new Person(71, "Alice", "Ben", new Address("London", "UK")));
+            repository.save(new Person(75, "Frank", "Ropourt", new Address("London", "UK")));
 
             logger.info("");
             logger.info("Count: " + repository.count());
@@ -57,6 +61,25 @@ public class Application {
 
             logger.info("== filter by country=US ==");
             repository.findByAddress_Country("US").forEach(Application::printPerson);
+
+            logger.info("== filter with pageable by country=Turkey ==");
+            // will caouse an extra query to count to calculate available pages
+            repository.findAllByAddress_Country("Turkey", new PageRequest(0, 2))
+                    .forEach(Application::printPerson);
+
+            logger.info("== filter with pageable without counting by country=Turkey ==");
+            repository.findAllWithoutCountByAddress_Country("Turkey", new PageRequest(0, 2))
+                    .forEach(Application::printPerson);
+
+            logger.info("== findAllByHeight with slices ==");
+            repository.findAllByHeight(75, new PageRequest(0, 1))
+                    .forEach(Application::printPerson);
+
+            logger.info("== the tallest person ==");
+            printPerson(repository.findTopByOrderByHeightDesc());
+
+            logger.info("== the tallest 3 person ==");
+            repository.findTop3ByOrderByHeightDesc().forEach(Application::printPerson);
         };
     }
 
