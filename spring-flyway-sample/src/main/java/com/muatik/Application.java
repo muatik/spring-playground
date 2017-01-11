adaa55686c4fda6444874acfb356371da647e47c
muatik <muatik@gmail.com>
2017-01-11 13:23:19 +0200

    adds hibernate auto-ddl options to spring-flyway-sample

--- spring-flyway-sample/src/main/java/com/muatik/Application.java
+++ spring-flyway-sample/src/main/java/com/muatik/Application.java
@@ -1,7 +1,10 @@
 package com.muatik;
 
+import com.muatik.repository.ProductsRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 /**
  * Created by muatik on 1/11/17.
@@ -11,5 +14,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    @Bean
+    public CommandLineRunner commandLineRunner(ProductsRepository repository) {
+        return (args) -> {
+          repository.findAll();
+        };
     }
 }
