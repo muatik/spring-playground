1c8d010926a017f1b7a7db8f394e07493a952e83
Mustafa Atik <muatik@gmail.com>
2017-01-18 17:09:11 +0200

    fixes filling property variables in spring-profiles

--- spring-profiles-example1/src/main/java/com/muatik/Application.java
+++ spring-profiles-example1/src/main/java/com/muatik/Application.java
@@ -1,29 +1,26 @@
 package com.muatik;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.ApplicationContext;
+
 
 /**
  * Created by mustafaatik on 18/01/17.
  */
 @SpringBootApplication
-@EnableConfigurationProperties(ConfigBinder.class)
 public class Application {
-
-    @Value("${my.name}")
-    private String name;
-
-    @Value("${spring.jpa.show-sql}")
-    private boolean showSql;
 
     public static void main(String[] args) {
         final ApplicationContext ctx = SpringApplication.run(Application.class, args);
         final ConfigBinder confs = ctx.getBean(ConfigBinder.class);
+        final ConfigValueBinder confs2 = ctx.getBean(ConfigValueBinder.class);
+
         System.out.println(confs.getUrl());
         System.out.println(confs.getName());
+
+        System.out.println(confs2.getUrl());
+        System.out.println(confs2.getName());
     }
 
 }
--- spring-profiles-example1/src/main/java/com/muatik/ConfigBinder.java
+++ spring-profiles-example1/src/main/java/com/muatik/ConfigBinder.java
@@ -1,16 +1,16 @@
 package com.muatik;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
 
 
 /**
  * Created by mustafaatik on 18/01/17.
  */
+@Component
 @ConfigurationProperties(prefix="my")
 public class ConfigBinder {
 
-    @Value("${my.name}")
     private String name;
 
     private String url; // expected to be filled automatically
@@ -21,5 +21,12 @@ public class ConfigBinder {
 
     public String getName() {
         return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    public void setUrl(String url) {
+        this.url = url;
     }
 }
